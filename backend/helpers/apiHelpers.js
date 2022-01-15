563e11248839c9456e32d1712a66153e023e2574
Olha Dudchenko <dudchenko.olha92@gmail.com>
2022-01-15 23:10:56 +0200

    add helpers

---
+++ backend/helpers/apiHelpers.js
@@ -0,0 +1,25 @@
+const { AppError } = require('../helpers/errors');
+
+const asyncWrapper = (controller) => {
+  return async(req, res, next) => {
+    try {
+      await controller(req, res,next)
+    } catch (error) {
+      next(error)
+    }
+    
+  }
+};
+
+const errorHandler = (error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.status).json({ message: error.message })
+  }
+  
+ res.status(500).json({ message: error.message })
+};
+
+module.exports = {
+  asyncWrapper,
+  errorHandler
+};

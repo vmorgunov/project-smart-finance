38d876994662385ac031684cbb42271909e833f3
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2022-01-16 15:07:07 +0100

    Update ReportView.jsx

--- frontend/src/views/ReportView/ReportView.jsx
+++ frontend/src/views/ReportView/ReportView.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import MonthPicker from '../../components/MonthPicker/MonthPicker';
-
+import { ReportStatistic } from '../../components/ReportStatistic'
 import {
     ReportContainer,
     ReportNav,
     ReportComponent,
-    ReportCostsAndIncome,
     ReportCostsOrIncomeList,
     ReportGraph
 } from './ReportView.styled'
 
 function ReportView() {
+    // const [costs, setCosts] = useState([]);
+    // const [income, setIncome] = useState([]);
 return (
     <ReportContainer>
         <ReportNav>
@@ -23,8 +24,11 @@ return (
             
         </ReportNav>
 
-        <ReportCostsAndIncome>25.  "Расходы: - 18 000.00 грн. / Доходы: + 45 000.00 грн."</ReportCostsAndIncome>
-
+        <ReportStatistic>
+            {/* costs={costs}
+            income={income} */}
+        </ReportStatistic>
+        
         <ReportCostsOrIncomeList>
             <div>28. Стрелки в секциях "Расходы" и "Доходы"</div>
             <div>26. Разметка и стили секции "Расходы" / 27. Разметка и стили секции "Доходы"</div>

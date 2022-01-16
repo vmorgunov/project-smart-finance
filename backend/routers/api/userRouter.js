b20e7345880aefd72c8ba87a3751a64bb417eb62
Olha Dudchenko <dudchenko.olha92@gmail.com>
2022-01-16 18:33:10 +0200

    add users routes

--- backend/routers/api/userRouter.js
+++ backend/routers/api/userRouter.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { asyncWrapper } = require('../../helpers');
 const {UserController} = require('../../contollers');
+const{authMiddleware} = require('../../middlewares')
 
 
 router.post('/register', asyncWrapper(UserController.register));
+router.post('/login',  asyncWrapper(UserController.logIn));
+router.post('/logout', authMiddleware, asyncWrapper(UserController.logOut));
 
 module.exports = router;

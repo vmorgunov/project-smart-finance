c13b35792ea266f2426dcb305cf4749e818655ab
Olha Dudchenko <dudchenko.olha92@gmail.com>
2022-01-16 18:34:40 +0200

    update registration service

--- backend/services/users/registration.js
+++ backend/services/users/registration.js
@@ -1,21 +1,28 @@
 
+const jwt = require('jsonwebtoken');
 const { User } = require('../../models');
 const { RegistrationConflictError } = require('../../helpers');
+require('dotenv').config();
 
-const registrationUser = async ({ email, password }) => {
-  const findUserByEmail = await User.findOne({ email })
+const userRegister = async ({ email, password }) => {
+  const findUserByEmail = await User.findOne({ email });
   if (findUserByEmail) {
     throw new RegistrationConflictError('Email in use')
   }
+ 
+  const user = new User({ email, password});
+  await user.save();
 
-  const user = new User({ email, password })
-  await user.save()
+  const shortToken = jwt.sign({ _id: user._id }, process.env.ACCES_TOKEN_SECRET, { expiresIn: '1h' });
+  await User.findByIdAndUpdate(user._id,{ shortToken });
 
   const newUser = {
+    shortToken,
     email: user.email,
+    balance:user.balance,
   }
 
   return newUser
 };
 
-module.exports = registrationUser;
+module.exports = userRegister;

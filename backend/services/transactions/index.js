85f82aa35da9a073e8fe8e6d4a955ce2b57465ee
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2022-01-16 23:58:54 +0200

    fix bugs

--- backend/services/transactions/index.js
+++ backend/services/transactions/index.js
@@ -1,10 +1,7 @@
 const getMonthTransactions = require('./getMonthTransactions');
-const getMonthTransactionsSum = require('./getMonthTransactionsSum');
 const addTransaction = require('./addTransaction');
 
 module.exports = {
   getMonthTransactions,
-  getMonthTransactionsSum,
   addTransaction,
-
 };

ab34267493f771039fcc78c2a08ee5cea25b4ba4
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2022-01-17 17:24:12 +0100

    Update ReportStatistic.jsx

--- frontend/src/components/ReportStatistic/ReportStatistic.jsx
+++ frontend/src/components/ReportStatistic/ReportStatistic.jsx
@@ -3,28 +3,12 @@ import React from 'react';
 import { Container, Title, Span, SpanRed, Line } from './ReportStatistic.styled';
 
 export const ReportStatistic = ({ costs, income }) => {
-  let totalCosts = 0;
-  let totalIncome = 0;
-
-  if (costs) {
-    const sum = costs.reduce((acc, amount) => {
-      return acc + amount.total;
-    }, 0);
-    totalCosts = sum;
-  }
-
-  if (income) {
-    const sum = income.reduce((acc, amount) => {
-      return acc + amount.total;
-    }, 0);
-    totalIncome = sum;
-  }
-
+  
   return (
     <Container>
-      <Title>Costs:{<SpanRed>- {totalCosts} UAN</SpanRed>} </Title>
+      <Title>Расходы:{<SpanRed>- {costs} грн.</SpanRed>} </Title>
       <Line />
-      <Title>Income:{<Span>+ {totalIncome} UAN</Span>} </Title>
+      <Title>Доходы:{<Span>+ {income} грн.</Span>} </Title>
     </Container>
   );
 };

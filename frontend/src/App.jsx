ff7c72db52ab121c11ecca17705496bd8dc4a0c3
Vitalii Morgunov <utlab13@gmail.com>
2022-01-17 20:58:18 +0100

    Change route path for transaction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
-import { AppBar } from "./components/AppBar";
+import { AppBar } from './components/AppBar';
 
-import AuthView from "./views/AuthView/AuthView.jsx";
-import TransactionsView from "./views/TransactionsView/TransactionsView.jsx";
-import ReportView from "./views/ReportView/ReportView.jsx";
+import AuthView from './views/AuthView/AuthView.jsx';
+import TransactionsView from './views/TransactionsView/TransactionsView.jsx';
+import ReportView from './views/ReportView/ReportView.jsx';
 
-import {
-    Container
-} from "./App.styled.jsx";
+import { Container } from './App.styled.jsx';
 
 export const App = () => {
-    return (
-        <Container>
-            <AppBar />
+  return (
+    <Container>
+      <AppBar />
 
-            <Routes>
-                <Route exact path="/" element={<AuthView />} />
-                <Route exact path="/report" element={<TransactionsView />} />
-                <Route exact path="/report" element={<ReportView />} />
-            </Routes>
-
-        </Container>
-    );
-}
+      <Routes>
+        <Route exact path="/" element={<AuthView />} />
+        <Route exact path="/transaction" element={<TransactionsView />} />
+        <Route exact path="/report" element={<ReportView />} />
+      </Routes>
+    </Container>
+  );
+};

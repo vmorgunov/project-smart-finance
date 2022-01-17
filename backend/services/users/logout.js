f9048ac5a35b18efac319f107c52103508a00ae2
Olha Dudchenko <dudchenko.olha92@gmail.com>
2022-01-17 19:15:36 +0200

    update services

--- backend/services/users/logout.js
+++ backend/services/users/logout.js
@@ -1,10 +1,10 @@
 const { User } = require('../../models');
-const { NotAuthorizedError } = require('../../helpers');
+const { AppError } = require('../../helpers');
 
 const userLogout = async ({ _id }, shortToken) => {
   const user = await User.findOne({ _id });
   if (!user) {
-    throw new NotAuthorizedError(`User with id : '${_id}' not found`);
+    throw AppError.NotAuthorizedError(`User with id : '${_id}' not found`);
   }
   await User.findByIdAndUpdate(_id, { shortToken: null });
 };

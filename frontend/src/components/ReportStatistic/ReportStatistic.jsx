82031816edca05d561bd7742ad513be4ba4fdf1c
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2022-01-19 23:27:35 +0100

    Change view-file

--- frontend/src/components/ReportStatistic/ReportStatistic.jsx
+++ frontend/src/components/ReportStatistic/ReportStatistic.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React   from 'react';
+// import { useDispatch } from 'react-redux';
+// import { useSelector } from 'react-redux';
+
 import { Container, Title, Span, SpanRed, Line } from './ReportStatistic.styled';
 
-export const ReportStatistic = ({ costs, income }) => {
+export const ReportStatistic = () => {
+//   function numberWithCommas(x) {
+//   return parseFloat(x)
+//     .toFixed(2)
+//     .toString()
+//     .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ' ');
+// }
+//   const dispatch = useDispatch();
+//   const costs = useSelector();
+//   const income = useSelector();
+
+  // useEffect(() => {
+  //   dispatch();
+  //   dispatch();
+  // }, [dispatch]);
+
   return (
     <Container>
-      <Title>Расходы:{<SpanRed>- {costs} грн</SpanRed>} </Title>
+      <Title>Расходы:{<SpanRed>-{} грн</SpanRed>} </Title>
         <Line />
-      <Title>Доходы:{<Span>+ {income} грн</Span>} </Title>
+      <Title>Доходы:{<Span>+ {} грн</Span>} </Title>
     </Container>    
   );
 };

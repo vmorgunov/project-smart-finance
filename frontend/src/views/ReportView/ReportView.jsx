95e5bf8f62d38b1db598d5221342118cf9c18d97
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-01-21 00:17:44 +0100

    MP-add-moment-nav-RW-state-links

--- frontend/src/views/ReportView/ReportView.jsx
+++ frontend/src/views/ReportView/ReportView.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { useMediaQuery } from 'react-responsive'
-import { ReportButton } from '../../components'
+import {React, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { ReportButton, MonthPicker, Reports, ReportStatistic } from '../../components';
 // import {Balance} from '../../components'
-import { MonthPicker }   from '../../components';
-import { Reports } from '../../components';
-import { ReportStatistic } from '../../components';
 
-// import {ModalOutMobile} from '../../components' 
-// import { ModalOut } from '../../components';
+import 'moment/locale/ru';
+import moment from 'moment';
+
 import {
     ReportContainer,
     ReportHeader,
@@ -15,33 +13,60 @@ import {
 } from './ReportView.styled'
 
 export const ReportView = () => {
-const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 767 });
-const isTabletOrDesktop = useMediaQuery({ minWidth: 768 });
+    const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 767 });
+    const isTabletOrDesktop = useMediaQuery({ minWidth: 768 });
+    
+    const [newDate, setNewDate] = useState(moment(new Date()));
+    const [dateMonth, setDateMonth] = useState(moment(new Date()).format('MM'));
+    const [dateYears, setDateYears] = useState(moment(new Date()).format('YYYY'));
+
+    const switchMonthLeft = () => {
+        setDateMonth(newDate.add(-1, 'month').format('MM'));
+        if (dateMonth === '01') {
+            setDateYears(newDate.add('year').format('YYYY'));
+        }
+    };
+
+    const switchMonthRight = () => {
+        setDateMonth(newDate.add(1, 'month').format('MM'));
+        if (dateMonth === '12') {
+            setDateYears(newDate.add('year').format('YYYY'));
+        }
+    };
     
     return (
         <>
             <ReportContainer> 
-                
                 {isMobile &&
                     <>
                         <ReportButton />                   
                         <MonthPicker />
                     </>
                 }
-
                 {isTabletOrDesktop &&
                     <ReportHeader>
+
                         <ReportButton />
+
                         {/* <Balance/> */}
-                        <MonthPicker />
+
+                        <MonthPicker
+                            switchMonthLeft={switchMonthLeft}
+                            switchMonthRight={switchMonthRight}
+                            dateMonth={dateMonth}
+                            dateYears={dateYears}
+                        />
+
                     </ReportHeader>
                 }
                       {/* {isTabletOrDesktop && <ModalOut />}
                       {isMobile && <ModalOutMobile/>} */}
                 <ReportStatistic>
-                    </ReportStatistic>
-                    <Reports />
-                    <ReportGraph>29. График</ReportGraph>
+                </ReportStatistic>
+
+                <Reports />
+                
+                <ReportGraph>29. График</ReportGraph>
             </ReportContainer>
         </>
     );
